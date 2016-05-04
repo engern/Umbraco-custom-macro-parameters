ce475f7413c5d5a59718d67924c35589a2b774b1
Tommy Enger <tommy_enger@hotmail.com>
2016-05-04 12:04:26 +0200

    Added files via upload

---
+++ App_Plugins/MacroRichText/Editor.controller.js
@@ -0,0 +1,19 @@
+﻿angular.module("umbraco").controller("MacroRichText.EditorController", function ($scope) {
+
+    $scope.textInput = {
+        label: "bodyText",
+        description: "Load some stuff here",
+        view: "rte",
+        value: $scope.model.value,
+        config: {
+            editor: {
+                toolbar: ["code", "styleselect", "bold", "italic", "underline", "link", "p360link"],
+                stylesheets: ["Editor"],
+                dimensions: { height: 400 }
+            }
+        }
+    };
+    $scope.$watch("textInput.value", function (newValue, oldValue) {
+        $scope.model.value = newValue;
+    });
+});
